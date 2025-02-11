b891011f3fc799627c046a4ed0f682b9934450af
karthik <kollakarthik21@gmail.com>
2025-02-11 11:40:22 +0530

    msg2

--- shell.sh
+++ shell.sh
@@ -5,8 +5,7 @@ stash completed
 tag completed
 linux completd
 how to conect putty completd
-ec2 instace created
-.................
+ec2 instace createdW
 permissions created
 file created
 hard link and soft link
